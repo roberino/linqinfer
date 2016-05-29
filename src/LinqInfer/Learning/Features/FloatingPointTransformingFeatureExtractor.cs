5c44a3486f2fa7fcb1fb8f1e0603ce42c0e6d064
Rob Eyres <Rob Eyres>
2016-05-29 10:11:53 +0100

    Transforming feature extractor

---
+++ src/LinqInfer/Learning/Features/FloatingPointTransformingFeatureExtractor.cs
@@ -0,0 +1,17 @@
+﻿using LinqInfer.Maths;
+using System;
+
+namespace LinqInfer.Learning.Features
+{
+    internal class FloatingPointTransformingFeatureExtractor<TInput> : TransformingFeatureExtractor<TInput, double>, IFloatingPointFeatureExtractor<TInput>
+    {
+        public FloatingPointTransformingFeatureExtractor(IFloatingPointFeatureExtractor<TInput> baseFeatureExtractor, Func<double[], double[]> transformation, int[] indexSelection = null) : base(baseFeatureExtractor, transformation, indexSelection)
+        {
+        }
+
+        public ColumnVector1D ExtractColumnVector(TInput obj)
+        {
+            return new ColumnVector1D(base.ExtractVector(obj));
+        }
+    }
+}
