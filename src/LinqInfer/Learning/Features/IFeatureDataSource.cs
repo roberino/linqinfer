c853a8df00e723cf6a7ef9648f2a45a4612fa08a
Eyres <robert.eyres@euromoneyplc.com>
2016-06-12 09:12:22 +0100

    Version increase + missing files

---
+++ src/LinqInfer/Learning/Features/IFeatureDataSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using LinqInfer.Maths;
+
+namespace LinqInfer.Learning.Features
+{
+    public interface IFeatureDataSource
+    {
+        /// <summary>
+        /// Returns the vector size of the feature vector.
+        /// </summary>
+        int VectorSize { get; }
+
+        /// <summary>
+        /// Returns an enumeration of feature metadata.
+        /// </summary>
+        IEnumerable<IFeature> FeatureMetadata { get; }
+
+        /// <summary>
+        /// Returns an enumeration of extracted vector data.
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<ColumnVector1D> ExtractVectors();
+    }
+}
---
+++ src/LinqInfer/Learning/Features/IFeatureTransformBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using LinqInfer.Data;
+using System.Collections.Generic;
+
+namespace LinqInfer.Learning.Features
+{
+    public interface IFeatureTransformBuilder<T> where T : class
+    {
+        IFloatingPointFeatureExtractor<T> FeatureExtractor { get; }
+        IEnumerable<IBlobStore> Outputs { get; }
+        FeatureProcessingPipline<T> FilterFeatures(Func<IFeature, bool> featureFilter);
+        FeatureProcessingPipline<T> FilterFeaturesByProperty(Action<PropertySelector<T>> selector);
+        FeatureProcessingPipline<T> PreprocessWith(Func<double[], double[]> transformFunction);
+        FeatureProcessingPipline<T> OutputResultsTo(IBlobStore store);
+    }
+}
