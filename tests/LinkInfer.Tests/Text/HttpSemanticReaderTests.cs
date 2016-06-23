0f42b658132c7f36dff31ad3b89e2bc4a44fabb3
Eyres <robert.eyres@euromoneyplc.com>
2016-06-23 09:20:21 +0100

    HTML parser fixes + semantic reader

---
+++ tests/LinkInfer.Tests/Text/HttpSemanticReaderTests.cs
@@ -0,0 +1,27 @@
+﻿using LinqInfer.Text;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace LinqInfer.Tests.Text
+{
+    [TestFixture]
+    public class HttpSemanticReaderTests
+    {
+        [TestCase("http://localhost/test.html")]
+        public async Task Read_TestUrl_ReturnsTokenStream(string url)
+        {
+            var reader = new HttpSemanticReader();
+
+            await reader.Read(new Uri(url), x =>
+            {
+                foreach(var t in x.Item2)
+                {
+                    Console.WriteLine(t);
+                }
+
+                return true;
+            });
+        }
+    }
+}
