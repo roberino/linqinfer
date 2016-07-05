dec0f64a4197e1561969e1ed9a4b99323234ca7a
Eyres <robert.eyres@euromoneyplc.com>
2016-07-05 21:13:10 +0100

    Unsafe compile fix / version

--- build.cmd
+++ build.cmd
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m:8 /p:Configuration=Release "LinqInfer.sln"
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /p:AllowUnsafeBlocks=true /m:8 /p:Configuration=Release "LinqInfer.sln"
 PAUSE
