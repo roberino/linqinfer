863681bb358045a26d2bdfd34f035bc720f5e9ec
Eyres <robert.eyres@euromoneyplc.com>
2016-08-21 20:51:06 +0100

    Removed dotnet std

--- src/LinqInfer/Utility/TypeExtensions.cs
+++ src/LinqInfer/Utility/TypeExtensions.cs
@@ -7,23 +7,7 @@ namespace LinqInfer.Utility
     public static class TypeExtensions
     {
         private static readonly Type nullableType = typeof(Nullable<>);
-
-        public static T GetCustomAttributes<T>(this Type type)
-        {
-#if NET_STD
-            var typeInf = type.GetTypeInfo();
-
-            var attr = typeInf.CustomAttributes.FirstOrDefault(a => a.AttributeType == typeof(T));
-
-            if (attr != null) return (T)attr;
-
-            return default(T);
-#else
-
-#endif
-            return type.GetCustomAttributes<T>();
-        }
-
+        
         public static Type GetNullableTypeType(this Type type)
         {
             if (type.IsGenericType && type.GetGenericTypeDefinition() == nullableType)
