7ae83cd97b179208efff6555a689de0e55ec79d1
Eyres <robert.eyres@euromoneyplc.com>
2016-08-26 09:40:58 +0100

    Nuget pack changes

--- package.cmd
+++ package.cmd
@@ -1,2 +1,3 @@
-.nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
+rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
+dotnet pack "src\LinqInfer\project.json"
 PAUSE
