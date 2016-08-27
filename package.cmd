db77533599805df5a43b69623789d1ff14042940
Eyres <robert.eyres@euromoneyplc.com>
2016-08-27 09:12:17 +0100

    Package script updates

--- package.cmd
+++ package.cmd
@@ -1,3 +1,3 @@
 rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
-dotnet pack "src\LinqInfer\project.json"
+dotnet pack "src\LinqInfer\project.json" --output artifacts
 PAUSE
