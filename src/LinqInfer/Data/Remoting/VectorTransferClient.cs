b8b6d3f1e4b8dfc0f2de8c156af3ef4837868cd3
Eyres <robert.eyres@euromoneyplc.com>
2016-09-06 09:24:53 +0100

    Basic vector transfer server + client

---
+++ src/LinqInfer/Data/Remoting/VectorTransferClient.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqInfer.Data.Remoting
+{
+    internal class VectorTransferClient : IDisposable
+    {
+        private readonly EndPoint _endpoint;
+        private readonly Socket _socket;
+
+        public VectorTransferClient(int port = VectorTransferServer.DefaultPort, string host = "127.0.0.1")
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            _endpoint = VectorTransferServer.GetEndpoint(host, port);
+        }
+
+        public Task<TransferHandle> BeginTransfer(string operationType, Action<TransferHandle> onConnect = null)
+        {
+            var state = new TransferHandle(operationType, new SocketState(_socket), onConnect);
+            var connectedHandle = new ManualResetEvent(false);
+
+            return Task<TransferHandle>.Factory.StartNew(() =>
+            {
+                _socket.BeginConnect(_endpoint, a =>
+                {
+                    var handle = (TransferHandle)a.AsyncState;
+
+                    handle.State.ClientSocket.EndConnect(a);
+                    handle.OnConnect(handle);
+                    connectedHandle.Set();
+                }, state);
+
+                connectedHandle.WaitOne();
+
+                return state;
+            });
+        }
+
+        public void Dispose()
+        {
+            if (_socket.Connected)
+            {
+                try
+                {
+                    _socket.Close();
+                }
+                catch
+                {
+
+                }
+            }
+
+            _socket.Dispose();
+        }
+    }
+}
---
+++ tests/LinqInfer.Tests/Data/Remoting/VectorTransferServerTests.cs
@@ -0,0 +1,41 @@
+﻿using LinqInfer.Data.Remoting;
+using LinqInfer.Maths;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqInfer.Tests.Data.Remoting
+{
+    [TestFixture]
+    public class VectorTransferServerTests
+    {
+        [Test]
+        public async Task Server_ReceiveData()
+        {
+            using (var server = new VectorTransferServer())
+            {
+                server.AddHandler("x", d =>
+                {
+                    foreach(var vect in d.Vectors)
+                    {
+                        Console.WriteLine(vect);
+                    }
+                    return true;
+                });
+
+                server.Start();
+
+                var client = new VectorTransferClient();
+
+                var handle = await client.BeginTransfer("x");
+
+                var data = new[] { ColumnVector1D.Create(1, 2, 3), ColumnVector1D.Create(4, 5, 6) };
+
+                await handle.Send(data);
+
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
