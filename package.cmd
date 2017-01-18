da15da970ed1c5a1a9878f9b6cafa6b26f16bf56
Eyres <robert.eyres@euromoneyplc.com>
2017-01-18 22:21:00 +0000

    Owin project.json, performance tweaks

--- package.cmd
+++ package.cmd
@@ -1,5 +1,5 @@
 rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
-dotnet pack "src\LinqInfer\project.json" --output artifacts
+dotnet build "src\LinqInfer\project.json"
 dotnet pack "src\LinqInfer.Storage.SQLite\project.json" --output artifacts
 .nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
 .nuget\nuget pack "src\LinqInfer.Owin\LinqInfer.Owin.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
