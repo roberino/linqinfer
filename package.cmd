da9ba143bed3c6da8e116403d682f331912eddf4
Eyres <robert.eyres@euromoneyplc.com>
2017-01-31 22:38:06 +0000

    Build and pack fixes

--- package.cmd
+++ package.cmd
@@ -1,6 +1,7 @@
 rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
 dotnet build "src\LinqInfer\project.json"
+dotnet pack "src\LinqInfer.Data.Orm\project.json" --output artifacts
 dotnet pack "src\LinqInfer.Storage.SQLite\project.json" --output artifacts
-.nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
-.nuget\nuget pack "src\LinqInfer.Owin\LinqInfer.Owin.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
+.nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
+.nuget\nuget pack "src\LinqInfer.Owin\LinqInfer.Owin.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
 PAUSE
