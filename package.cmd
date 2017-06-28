81cc22c73a8a01151e175debe36de1f5ed1c20ec
Rob Eyres <roberino.dev@gmail.com>
2017-06-28 12:07:10 +0100

    Build script refactor

--- package.cmd
+++ package.cmd
@@ -1,7 +1,9 @@
 rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
-dotnet build "src\LinqInfer\project.json"
-dotnet pack "src\LinqInfer.Data.Orm\project.json" --output artifacts
-dotnet pack "src\LinqInfer.Storage.SQLite\project.json" --output artifacts
-.nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
+dotnet restore"src\LinqInfer\LinqInfer-dotnetcore.csproj"
+dotnet build "src\LinqInfer\LinqInfer-dotnetcore.csproj"
+dotnet pack "src\LinqInfer\LinqInfer-dotnetcore.csproj"
+rem dotnet pack "src\LinqInfer.Data.Orm\project.json" --output artifacts
+rem dotnet pack "src\LinqInfer.Storage.SQLite\project.json" --output artifacts
+rem .nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
 .nuget\nuget pack "src\LinqInfer.Owin\LinqInfer.Owin.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
 PAUSE
