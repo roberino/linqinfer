d235041b6607d00ca804b21cb505269a19a46e8d
Rob Eyres <roberino.dev@gmail.com>
2017-07-05 16:26:29 +0100

    Build script + test amendment

--- package.cmd
+++ package.cmd
@@ -1,9 +1,13 @@
-rem .nuget\nuget pack "src\LinqInfer\LinqInfer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive
+rem build main lib
+
 dotnet restore "src\LinqInfer\LinqInfer-dotnetcore.csproj"
 dotnet build "src\LinqInfer\LinqInfer-dotnetcore.csproj"
 dotnet pack "src\LinqInfer\LinqInfer-dotnetcore.csproj" --output ..\..\artifacts
-rem dotnet pack "src\LinqInfer.Data.Orm\project.json" --output artifacts
-rem dotnet pack "src\LinqInfer.Storage.SQLite\project.json" --output artifacts
-rem .nuget\nuget pack "src\LinqInfer.NeuralServer\LinqInfer.NeuralServer.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
-rem .nuget\nuget pack "src\LinqInfer.Owin\LinqInfer.Owin.csproj" -OutputDirectory artifacts -IncludeReferencedProjects -NonInteractive -Prop Configuration=Release
+
+rem build aspnet core lib
+
+dotnet restore "src\LinqInfer.AspNetCore\LinqInfer.AspNetCore.csproj"
+dotnet build "src\LinqInfer.AspNetCore\LinqInfer.AspNetCore.csproj"
+dotnet pack "src\LinqInfer.AspNetCore\LinqInfer.AspNetCore.csproj" --output ..\..\artifacts
+
 PAUSE
