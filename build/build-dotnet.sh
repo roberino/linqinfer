4f70b24becce5b211646aae08abf0f56cb43dde0
Rob Eyres <roberino.dev@gmail.com>
2017-07-10 23:33:39 +0100

    Win build fixes / tweaks / version update

--- build/build-dotnet.sh
+++ build/build-dotnet.sh
@@ -1,7 +1,7 @@
-dotnet restore -f netstandard2.0 "../src/LinqInfer/LinqInfer-dotnetcore.csproj"
+dotnet restore "../src/LinqInfer/LinqInfer-dotnetcore.csproj"
 dotnet build -f netstandard2.0 "../src/LinqInfer/LinqInfer-dotnetcore.csproj"
 
-dotnet restore -f netstandard1.6 "../tests/LinqInfer.Tests/LinqInfer.Tests-dotnetcore.csproj"
+dotnet restore "../tests/LinqInfer.Tests/LinqInfer.Tests-dotnetcore.csproj"
 dotnet build -f netstandard1.6 "../tests/LinqInfer.Tests/LinqInfer.Tests-dotnetcore.csproj"
 
 dotnet restore "../tests/LinqInfer.AspNetCoreTestHarness/LinqInfer.AspNetCoreTestHarness.csproj"
