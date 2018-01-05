fc597909fdd748a6629413c00c7475a630fe1628
Robert Eyres <Rob@localhost.localdomain>
2018-01-05 18:31:15 +0000

    Docker fixes

--- tests/Scripts/setup-text-server.sh
+++ tests/Scripts/setup-text-server.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-PORT="8083"
+PORT="8091"
 
 curl -X DELETE -H "Content-Type: application/json; charset=UTF-8" http://localhost:$PORT/text/indexes/i1/
 
 curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "{}" http://localhost:$PORT/text/indexes/i1/
 
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-m1.json" http://localhost:$PORT/text/indexes/i1/documents/m1
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-m2.json" http://localhost:$PORT/text/indexes/i1/documents/m2
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-m3.json" http://localhost:$PORT/text/indexes/i1/documents/m3
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-m4.json" http://localhost:$PORT/text/indexes/i1/documents/m4
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-s1.json" http://localhost:$PORT/text/indexes/i1/documents/s1
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-s2.json" http://localhost:$PORT/text/indexes/i1/documents/s2
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-s3.json" http://localhost:$PORT/text/indexes/i1/documents/s3
-curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-s4.json" http://localhost:$PORT/text/indexes/i1/documents/s4
+for i in {1..5}
+do
+	curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-m$i.json" http://localhost:$PORT/text/indexes/i1/documents/m$i
+done
 
+for i in {1..5}
+do
+	curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./doc-s$i.json" http://localhost:$PORT/text/indexes/i1/documents/s$i
+done
 
 curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./classifier1.json" http://localhost:$PORT/text/indexes/i1/classifiers/c1 -v
 curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./classifier2.json" http://localhost:$PORT/text/indexes/i1/classifiers/c2 -v
+curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./classifier3.json" http://localhost:$PORT/text/indexes/i1/classifiers/c3 -v
+curl -X POST -H "Content-Type: application/json; charset=UTF-8" --data "@./classifier4.json" http://localhost:$PORT/text/indexes/i1/classifiers/c4 -v
 
 
