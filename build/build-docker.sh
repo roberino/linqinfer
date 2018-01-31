1ae61048d9b7d004c831d430183f9ce64e556429
Rob Eyres <rob.eyres@asos.com>
2018-01-31 09:44:01 +0000

    Build path fixes

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -1,8 +1,8 @@
 dotnet restore "../src/LinqInfer/LinqInfer.csproj"
 dotnet build -f netstandard2.0 "../src/LinqInfer/LinqInfer.csproj"
 
-dotnet restore "../tests/LinqInfer.Tests/LinqInfer.Tests.csproj"
-dotnet build -f netcoreapp1.1 "../tests/LinqInfer.Tests/LinqInfer.Tests.csproj"
+dotnet restore "../tests/LinqInfer.Tests/LinqInfer.UnitTests.csproj"
+dotnet build -f netcoreapp1.1 "../tests/LinqInfer.Tests/LinqInfer.UnitTests.csproj"
 
 dotnet restore "../src/LinqInfer.Microservices/LinqInfer.Microservices.csproj"
 dotnet build -f netcoreapp1.1 "../src/LinqInfer.AspNetCore/LinqInfer.AspNetCore.csproj"
--- build/build-dotnet.sh
+++ build/build-dotnet.sh
@@ -1,8 +1,8 @@
 dotnet restore "../src/LinqInfer/LinqInfer.csproj"
 dotnet build -f netstandard2.0 "../src/LinqInfer/LinqInfer.csproj"
 
-dotnet restore "../tests/LinqInfer.Tests/LinqInfer.Tests.csproj"
-dotnet build -f netstandard1.6 "../tests/LinqInfer.Tests/LinqInfer.Tests.csproj"
+dotnet restore "../tests/LinqInfer.Tests/LinqInfer.UnitTests.csproj"
+dotnet build -f netstandard1.6 "../tests/LinqInfer.Tests/LinqInfer.UnitTests.csproj"
 
 dotnet restore "../tests/LinqInfer.AspNetCoreTestHarness/LinqInfer.AspNetCoreTestHarness.csproj"
 dotnet build "../tests/LinqInfer.AspNetCoreTestHarness/LinqInfer.AspNetCoreTestHarness.csproj"
