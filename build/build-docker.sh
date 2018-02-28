f05e7a58d4de7975a3fafaf52b825d9407481d5e
Robert Eyres <Rob@localhost.localdomain>
2018-02-28 07:46:50 +0000

    Docker fixes

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -1,3 +1,7 @@
-dotnet restore "../tests/LinqInfer.Tests/LinqInfer.UnitTests.csproj"
+dotnet restore "../tests/LinqInfer.UnitTests/LinqInfer.UnitTests.csproj"
+dotnet restore "../tests/LinqInfer.Benchmarking/LinqInfer.Benchmarking.csproj"
+
 dotnet build -c Release -f netcoreapp2.0 "../tests/LinqInfer.UnitTests/LinqInfer.UnitTests.csproj"
 dotnet build -c Release -f netcoreapp2.0 "../tests/LinqInfer.Benchmarking/LinqInfer.Benchmarking.csproj"
+
+dotnet publish -c Release -f netcoreapp2.0 -o "../tests/Benchmarks" "../tests/LinqInfer.Benchmarking/LinqInfer.Benchmarking.csproj"
