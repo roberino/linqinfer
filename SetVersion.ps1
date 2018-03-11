293429450275af1d03840ca2ad19162608450992
Rob Eyres <rob.eyres@asos.com>
2018-03-11 19:44:18 +0000

    Integrate git version

---
+++ SetVersion.ps1
@@ -0,0 +1,16 @@
+$version = GitVersion | ConvertFrom-Json
+# Write-Host $version
+$currentPath = Split-Path $MyInvocation.MyCommand.Path
+$path = "$currentPath/src/LinqInfer/LinqInfer.csproj"
+$xml = [XML](Get-Content $path)
+$nodes = $xml.Project.PropertyGroup.ChildNodes
+
+foreach($node in $nodes) 
+{
+	if($node.Name -eq 'Version') 
+	{
+		Write-Host "Set" $node.Name "to" $version.NuGetVersionV2
+		$node.InnerText = $version.NuGetVersionV2
+	}
+}
+$xml.Save($path)
