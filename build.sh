5f93b5ba749dc1c107b358547683f6b25603ed03
Rob Eyres <rob.eyres@asos.com>
2018-09-10 18:14:20 +0100

    Versioning

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 dotnet restore "LinqInfer.sln"
 dotnet build "LinqInfer.sln"
 dotnet pack "src/LinqInfer/LinqInfer.csproj" --output ../../artifacts
-#dotnet pack "src/LinqInfer.Microservices/LinqInfer.Microservices.csproj" --output ../../artifacts
+# dotnet pack "src/LinqInfer.Microservices/LinqInfer.Microservices.csproj" --output ../../artifacts
